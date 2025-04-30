99a3c95af367efc035f1f986520df7b36bf59176
DnilsonAH <dnilson.achahuanco@tecsup.edu.pe>
2025-04-30 13:42:30 -0500

    Initial Commit

---
+++ TableViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewController
+//
+//  Created by Dnilson Achahuanco on 30/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
